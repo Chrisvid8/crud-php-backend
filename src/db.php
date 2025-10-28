8a907855789fd6f77d0b276ca43907c04cfe2a86
Christian David <davidchristiannoe@gmail.com>
2025-10-29 01:58:45 +0800

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = '127.0.0.1';
-$db   = 'crud_db';
-$user = 'root';
-$pass = '';
+$host = $_ENV['DB_HOST'] ?? '127.0.0.1';
+$db   = $_ENV['DB_NAME'] ?? 'crud_db';
+$user = $_ENV['DB_USER'] ?? 'root';
+$pass = $_ENV['DB_PASS'] ?? '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
